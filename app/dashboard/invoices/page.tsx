84bdfebab3ec9ecb0f73b437da6fbdff841b17f4
Yash Sood <yashsood.dev@gmail.com>
2024-01-28 00:35:45 +0530

    chapter 16: Metadata completed

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,11 +1,16 @@
 import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
+import { Metadata } from 'next';
 import Table from '@/app/ui/invoices/table';
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 
 export default async function Page({
   searchParams,
